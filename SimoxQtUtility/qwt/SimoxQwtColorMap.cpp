b2627fea2040d69908b926c3c72b3f6dba4a4860
Christoph Pohl <christoph.pohl@kit.edu>
2024-01-03 13:51:52 +0100

    Fix missing function definition

--- SimoxQtUtility/qwt/SimoxQwtColorMap.cpp
+++ SimoxQtUtility/qwt/SimoxQwtColorMap.cpp
@@ -1,34 +1,39 @@
 #include "SimoxQwtColorMap.h"
+
 #include <qwt_interval.h>
 
 namespace simox::qt
 {
-    SimoxQwtColorMap::SimoxQwtColorMap(const std::string& n):
+    SimoxQwtColorMap::
+    SimoxQwtColorMap(const std::string& n) :
         SimoxQwtColorMap(simox::color::cmaps::Named::get(n))
     {
     }
 
-    SimoxQwtColorMap::SimoxQwtColorMap(const color::ColorMap& cm):
+    SimoxQwtColorMap::
+    SimoxQwtColorMap(const color::ColorMap& cm) :
         cm{cm}
     {
     }
 
-    QRgb SimoxQwtColorMap::rgb(const QwtInterval& interval, double value) const
+    QRgb
+    SimoxQwtColorMap::rgb(const QwtInterval& interval, double value) const
     {
-        const auto c = cm.at(value,
-                             interval.minValue(),
-                             interval.maxValue());
+        const auto c = cm.at(value, interval.minValue(), interval.maxValue());
         return QColor{c.r, c.g, c.b}.rgb();
     }
 
 #if QWT_VERSION >= 0x060200
-    uint SimoxQwtColorMap::colorIndex(int numColors,
-                                 const QwtInterval& interval, double value) const
+    uint
+    SimoxQwtColorMap::colorIndex(int /*numColors*/,
+                                 const QwtInterval& /*interval*/,
+                                 double /*value*/) const
 #else
-    unsigned char colorIndex(const QwtInterval& interval, double value)
+    unsigned char
+    SimoxQwtColorMap::colorIndex(const QwtInterval& /*interval*/, double /*value*/) const
 #endif
     {
         return 0;
     }
 
-}
+} // namespace simox::qt
