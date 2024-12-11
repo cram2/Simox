470deba401f7b7dda50c4172230f19bcfd81b649
Fabian Reister <fabian.reister@kit.edu>
2024-12-11 15:14:31 +0100

    cleanup

--- VirtualRobot/VirtualRobot.h
+++ VirtualRobot/VirtualRobot.h
@@ -281,10 +281,6 @@ namespace VirtualRobot
 
 } // namespace
 
-
-// FIXME remove
-#include <iostream>
-
 #define VR_INFO std::cout <<__FILE__ << ":" << __LINE__ << ": "
 #define VR_WARNING std::cerr <<__FILE__ << ":" << __LINE__ << " -Warning- "
 #define VR_ERROR std::cerr <<__FILE__ << ":" << __LINE__ << " - ERROR - "
